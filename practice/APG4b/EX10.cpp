bfdb306a8f9fc8060d6d32726aa657d4273e2b3a
mo-mo- <mo-mo-666.pu_ki@outlook.com>
2019-12-20 04:16:55 +0900

    WIP APG4b

---
+++ practice/APG4b/EX10.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    cout << "A:";
+    for (int i = 0; i < A; i++) {
+        cout << "]";
+    }
+    cout << endl << "B:";
+
+    for (int j = 0; j < B; j++) {
+        cout << "]";
+    }
+    cout << endl;
+}
